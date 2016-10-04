b35d270ef828249cb9b208a7dd458ce18a7fbd7f
NiwradMendyke <mendyke@usc.edu>
2016-10-03 23:31:09 -0700

    adding messenger app

--- Messenger/Messenger/ChatViewController.swift
+++ Messenger/Messenger/ChatViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import Parse
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var messageField: UITextField!
+    
+    @IBOutlet weak var chatTable: UITableView!
+    
+    var messages : [PFObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        chatTable.dataSource = self
+        chatTable.estimatedRowHeight = 100
+        chatTable.rowHeight = UITableViewAutomaticDimension
         // Do any additional setup after loading the view.
+        
+        getMessages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +33,77 @@ class ChatViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //getMessages()
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //print("calling cellForRowAt")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell") as! MessageViewCell
+        
+        //getMessages()
+        let text = messages[messages.count - 1 - indexPath.row]
+        
+        let user = text["user"] as! String?
+        cell.messageLabel.text = user! + ":" + (text["text"] as! String?)!
+        
+        return cell
+    }
+    
+    @IBAction func sendMessage(_ sender: AnyObject) {
+        let message = PFObject(className:"Message_2016")
+        message["text"] = messageField.text!
+        message["user"] = PFUser.current()?.username
+        message.saveInBackground {
+            (success, error) -> Void in
+            if (success) {
+                print("Successfully sent: " + self.messageField.text!)
+                self.getMessages()
+            }
+            else {
+                print("Failed to send")
+            }
+        }
+    }
+    
+    func getMessages() {
+        print(PFUser.current()?.username)
+        
+        let query = PFQuery(className:"Message_2016")
+        //query.whereKey("user", equalTo:currentUser?.username)
+        query.findObjectsInBackground {
+            (objects: [PFObject]?, error: Error?) -> Void in
+            
+            if error == nil {
+                print("no error in retrieving search query")
+                // The find succeeded.
+                // Do something with the found objects
+                var temp_list = [PFObject]()
+                
+                print(objects?.count)
+                for object in objects! {
+                    if ((object["user"] as! String?) == (PFUser.current()?.username)) {
+                        temp_list.append(object)
+                    }
+                }
+                
+                print(temp_list.count)
+                temp_list.sorted(by: { $0.createdAt! > $1.createdAt! })
+                
+                self.messages = temp_list
+                print("filled messages")
+                print(self.messages.count)
+                self.chatTable.reloadData()
+            }
+            else {
+                // Log details of the failure
+                print("Could not get messages")
+            }
+        }
+        
+        print("calling reloadData")
+    }
 
     /*
     // MARK: - Navigation
--- Messenger/Messenger/LogInViewController.swift
+++ Messenger/Messenger/LogInViewController.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+import Parse
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if (PFUser.current() != nil) {
+            PFUser.logOut()
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -32,4 +41,60 @@ class LogInViewController: UIViewController {
     }
     */
 
+    @IBAction func signUpAction(_ sender: AnyObject) {
+        if (emailField.text == "" || passwordField.text == "") {
+            createAlert(message: "Both fields need input")
+            return
+        }
+        
+        let email = emailField.text
+        let password = passwordField.text
+        
+        let user = PFUser()
+        user.username = email
+        user.password = password
+       
+        user.signUpInBackground(block: { (succeed, error) -> Void in
+            // Stop the spinner
+            if ((error) != nil) {
+                self.createAlert(message: "Failed to sign up")
+                return
+            }
+            else {
+                print("signed up")
+                self.performSegue(withIdentifier: "FromSignUp", sender: nil)
+            }
+        })
+    }
+    
+    @IBAction func loginAction(_ sender: AnyObject) {
+        if (emailField.text == "" || passwordField.text == "") {
+            createAlert(message: "Both fields need input")
+            return
+        }
+        
+        let email = emailField.text
+        let password = passwordField.text
+        PFUser.logInWithUsername(inBackground: email!, password: password!, block: { (user, error) -> Void in
+            if (error != nil) {
+                self.createAlert(message: "Failed to log in")
+                return
+            }
+            else {
+                print(email! + " logged in successfully")
+                self.performSegue(withIdentifier: "FromLogIn", sender: nil)
+            }
+        })
+    }
+    
+    func createAlert(message: String) {
+        let alertController1 = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            // handle response here.
+        }
+        // add the OK action to the alert controller
+        alertController1.addAction(OKAction)
+        self.present(alertController1, animated: true)
+    }
 }
